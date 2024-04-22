8a0628e00fa0aabfb97ff10814be964add04bd1b
Joares <joaresmiranda@gmail.com>
2024-04-22 08:52:51 -0300

    add new feature to result: message and button to create new code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-const main = document.querySelector('#main');
+const container = document.querySelector('#container');
 const inputText = document.querySelector('#inputText');
 const buttonForm = document.querySelector('#button');
+const result = document.querySelector('#result');
 const qrCodeImg = document.querySelector('#result img');
+const textGenerate = document.querySelector('#textGenerate');
+const buttonNewQrCode = document.querySelector('#btnNewQrCode');
 
 // Function
 const generateQrCode = () => {
   const inputValue = inputText.value;
-  if (!inputValue) return;
 
-  buttonForm.value = 'Gerando código...';
+  if (!inputValue || inputValue.trim().length === 0) {
+    alert('Text or URL is required');
+    return;
+  }
+
+  buttonForm.value = 'Generating code...';
 
   qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=${inputValue}`;
 
   qrCodeImg.addEventListener('load', () => {
-    main.classList.add('active');
-    buttonForm.value = 'Código criado!';
+    container.classList.replace('active', 'inactive');
+    result.classList.replace('inactive', 'active');
+    textGenerate.innerHTML = `<p>${inputValue}</p>`;
   });
 };
 
@@ -30,9 +38,11 @@ inputText.addEventListener('keydown', (e) => {
   }
 });
 
-inputText.addEventListener('keyup', () => {
-  if (!inputText.value) {
-    buttonForm.value = 'Gerar QR-Code';
-    main.classList.remove('active');
-  }
+buttonNewQrCode.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.replace('inactive', 'active');
+  result.classList.replace('active', 'inactive');
+  buttonForm.value = 'Genarate QR-Code';
+  inputText.value = '';
+  inputText.focus();
 });
